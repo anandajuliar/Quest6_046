7e3624720f0ee568be3f4b34beea40b2e8fc2bb1
anandajuliar <anandajulia001@gmail.com>
2025-11-21 16:04:35 +0700

    menambah objek

--- app/src/main/java/com/example/myarsitekturmvvm_046/model/DataJK.kt
+++ app/src/main/java/com/example/myarsitekturmvvm_046/model/DataJK.kt
@@ -1,4 +1,10 @@
 package com.example.myarsitekturmvvm_046.model
 
+import com.example.myarsitekturmvvm_046.R
+
 object DataJK {
+    val JenisJK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
